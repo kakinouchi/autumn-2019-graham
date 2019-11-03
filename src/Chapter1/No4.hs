fe6c312d8e5d0c4ce7f3de919bde2112c0723c40
kakinouchi <kakinouchi_6283@icloud.com>
2019-11-03 19:06:35 +0900

    impl qsortByDesc

--- src/Chapter1/No4.hs
+++ src/Chapter1/No4.hs
@@ -1,4 +1,8 @@
 module Chapter1.No4 (qsortByDesc) where
 
-qsortByDesc :: Num a => [a] -> [a]
-qsortByDesc _ = [5,4,3,2,1]
+qsortByDesc :: Ord a => [a] -> [a]
+qsortByDesc []     = []
+qsortByDesc (x:xs) = qsortByDesc larger ++ [x] ++ qsortByDesc smaller
+                     where
+                        smaller = [a | a <- xs, a <= x]
+                        larger  = [b | b <- xs, b >  x] 
