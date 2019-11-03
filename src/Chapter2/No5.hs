509b79b35884bb62ce0497e0a101c2d5bcf63203
kakinouchi <kakinouchi_6283@icloud.com>
2019-11-03 19:32:08 +0900

    fake impl for my init function

---
+++ src/Chapter2/No5.hs
@@ -0,0 +1,4 @@
+module Chapter2.No5 (myInit) where
+
+    myInit :: Num a => [a] -> [a]
+    myInit _ = [1,2,3,4]
---
+++ test/Chapter2/No5Spec.hs
@@ -0,0 +1,11 @@
+ module Chapter2.No5Spec (spec) where
+
+     import Test.Hspec
+     import Chapter2.No5
+    
+     spec :: Spec
+     spec = do
+         describe "function my init test:" $
+          it "myInit [1,2,3,4,5] should be [1,2,3,4]]" $
+           (myInit [1,2,3,4,5]) `shouldBe` [1,2,3,4]
+    
