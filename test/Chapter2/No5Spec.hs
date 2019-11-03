013345d9febfb7d7a3b754789e178bc842d0fc2d
kakinouchi <kakinouchi_6283@icloud.com>
2019-11-03 19:37:02 +0900

    add another test for my init

--- test/Chapter2/No5Spec.hs
+++ test/Chapter2/No5Spec.hs
@@ -5,7 +5,10 @@
     
      spec :: Spec
      spec = do
-         describe "function my init test:" $
-          it "myInit [1,2,3,4,5] should be [1,2,3,4]]" $
+         describe "function my init test:" $ do
+          it "myInit [1,2,3,4,5] should be [1,2,3,4]" $
            (myInit [1,2,3,4,5]) `shouldBe` [1,2,3,4]
+
+          it "myInit [1,2,3,4,6] should be [1,2,3,4]" $
+           (myInit [1,2,3,4,6]) `shouldBe` [1,2,3,4]
     
